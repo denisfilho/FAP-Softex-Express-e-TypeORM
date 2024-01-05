9c19e3f6fa9c95ec36638d3a4fc293d644023894
denisfilho <denisferreira93@hotmail.com>
2024-01-05 17:10:04 -0300

    Implementação do CRUD

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,4 +1,5 @@
 import { DataSource } from "typeorm";
+import { User } from "../entities/user";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -7,6 +8,8 @@ export const AppDataSource = new DataSource({
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  entities: [User],
+  synchronize: true,
 });
 
 //Espera o AppDataSource Inicializar
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ dotenv.config(); //precisa vim antes de importar qualquer coisa
 
 import "reflect-metadata";
 import { starDatabase } from "./database/data-source";
+import app from "./app";
 
 async function main() {
   try {
     await starDatabase();
     console.log(`Database initialized`);
+    app.listen(3003, () => {
+      console.log("Aplicação usando requisições http na porta 3003");
+    });
   } catch (error) {
     console.log(error, "Error initializing app");
   }
